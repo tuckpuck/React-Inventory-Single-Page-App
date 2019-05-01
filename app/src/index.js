89488cf40dc95b032f16308ef89211b965379bfc
tuckpuck <tucker.triggs@yahoo.com>
2019-05-01 10:05:18 -0600

    added menu structure to App.js

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,8 @@ import {
     render
 } from 'react-dom';
 import StorePicker from './components/StorePicker';
-import "./css/style.css";
+import App from './components/App';
+import './css/style.css';
 
 
-render( < StorePicker / > , document.querySelector('#main'));
+render( < App / >, document.querySelector('#main'));
