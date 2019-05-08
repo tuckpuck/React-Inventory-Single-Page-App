67e29fc885156043a18a6f3defb917bbcc563794
tuckpuck <tucker.triggs@yahoo.com>
2019-05-08 08:19:29 -0600

    /store/ is routed to App

--- app/src/components/Router.js
+++ app/src/components/Router.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StorePicker from "./StorePicker";
+import App  from "./App";
 
 const Router = () => (
         <BrowserRouter>
         <Switch>
         <Route exact path="/" component={StorePicker} />
-
+        <Route path="/store/:storeId" component={App} />
 
         </Switch>
         </BrowserRouter>
