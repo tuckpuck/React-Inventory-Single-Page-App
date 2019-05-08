20c88d375fcd2f67d98b6c558d5a19317173b654
tuckpuck <tucker.triggs@yahoo.com>
2019-05-08 08:24:24 -0600

    wildcard is routed to NotFound component

--- app/src/components/Router.js
+++ app/src/components/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StorePicker from "./StorePicker";
+import NotFound from "./NotFound";
 import App  from "./App";
 
 const Router = () => (
@@ -8,6 +9,7 @@ const Router = () => (
         <Switch>
         <Route exact path="/" component={StorePicker} />
         <Route path="/store/:storeId" component={App} />
+        <Route component={NotFound} />
 
         </Switch>
         </BrowserRouter>
