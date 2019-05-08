706c8f0027d22b022d25dac07c5b36305ec79893
tuckpuck <tucker.triggs@yahoo.com>
2019-05-08 08:04:17 -0600

    created router.js, importing react-router

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Header(props) {
+const Header = (props) => {
     return ( 
         <header className="top">
         <h1>
---
+++ app/src/components/Router.js
@@ -0,0 +1,2 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
