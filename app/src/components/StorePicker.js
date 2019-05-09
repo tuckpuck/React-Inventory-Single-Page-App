53758c0aacf8092e95394e6f6a4a27a7e4a79be3
tuckpuck <tucker.triggs@yahoo.com>
2019-05-09 08:50:32 -0600

    receiving data from input field

--- app/src/components/StorePicker.js
+++ app/src/components/StorePicker.js
@@ -4,12 +4,26 @@ import { getFunName } from "../helpers";
 
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+    goToStore = event => {
+        // Prevent page reload
+        event.preventDefault();
+        // Get text from an input field
+        console.log(this); 
+        // Change page based on form input
+        
+    }
+
     render() {
         return (
-        <form action="" className="store-selector">
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
             <h2>Please Enter a Store</h2>
-            <input type="text" required="required" placeholder="Store Name" 
-            defaultValue={getFunName()} />  
+            <input 
+                type="text" 
+                ref={this.myInput}
+                required
+                placeholder="Store Name" 
+                defaultValue={getFunName()} />  
             <button type="submit">Visit Store →</button> 
         </form>
         )
