15ccb40fa7e3ea1f0060437213993625e5a71383
tuckpuck <tucker.triggs@yahoo.com>
2019-05-10 14:32:41 -0600

    added html for add item form

--- app/src/components/AddFishForm.js
+++ app/src/components/AddFishForm.js
@@ -3,7 +3,26 @@ import React from 'react';
 class AddFishForm extends React.Component {
     render() {
         return (
-            <p>hi from Fish Form</p>
+            <form className="fish-edit" onSubmit={this.createFish}>
+            <input name="name" type="text" placeholder="Name" />
+            <input
+              name="price"
+              type="text"
+              placeholder="Price"
+            />
+            <select name="status">
+              <option value="available">Fresh!</option>
+              <option value="unavailable">Sold Out!</option>
+            </select>
+    
+            <textarea name="desc" placeholder="Desc" />
+            <input
+              name="image"
+              type="text"
+              placeholder="Image"
+            />
+            <button type="submit">+ Add Fish</button>
+          </form>
         )
     }
 }
