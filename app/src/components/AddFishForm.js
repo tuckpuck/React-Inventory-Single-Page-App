b78c68ae37f1bbaf99c97e42754f43a432eadc14
tuckpuck <tucker.triggs@yahoo.com>
2019-05-10 14:24:43 -0600

    added fish form component

---
+++ app/src/components/AddFishForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+    render() {
+        return (
+            <p>hi from Fish Form</p>
+        )
+    }
+}
+
+export default AddFishForm;
--- app/src/components/Inventory.js
+++ app/src/components/Inventory.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
     render() {
         return (
             <div className="Inventory">
-            Inventory!!!</div>
+                <h2>Inventory</h2>
+                <AddFishForm/>
+            </div>
         )
     }
 }
