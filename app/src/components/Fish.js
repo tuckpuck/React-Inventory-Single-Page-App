3870325c0d03cffbefa60989bfcc1a7a2efe1afa
tuckpuck <tucker.triggs@yahoo.com>
2019-05-11 08:32:52 -0600

    sample fish rendering to page

--- app/src/components/Fish.js
+++ app/src/components/Fish.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
     render() {
+        const image = this.props.details.image;
+        const name = this.props.details.name;
+        const price = this.props.details.price;
+        const desc = this.props.details.desc;
+        const status = this.props.details.status;
         return (
-            <div className="single-fish">
-                🐠
-            </div>
+            <li className="menu-fish">
+               <img src={image} alt={name}/>
+               <h3 className="fish-name">{name} 
+                   <span className="price">{formatPrice(price)}</span>
+               </h3>
+               <p>{desc}</p>
+               <button>Add to Cart</button>
+            </li>
         )
     }
 }
