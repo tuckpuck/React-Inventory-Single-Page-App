29a2554455ce75e92ee7409e720d514a248a9de2
tuckpuck <tucker.triggs@yahoo.com>
2019-05-13 09:29:53 -0600

    function passed down, updating order state

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -44,7 +44,12 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
                         {Object.keys(this.state.fishes).map((key) => (
-                            <Fish key={key} details={this.state.fishes[key]} />
+                            <Fish
+                                key={key}
+                                index={key}
+                                details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder}
+                            />
                         ))}
                     </ul>
                 </div>
