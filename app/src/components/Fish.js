a6e47f4ad638778e690532616a31afaa37798c3e
tuckpuck <tucker.triggs@yahoo.com>
2019-05-13 08:36:49 -0600

    destructuring refactor

--- app/src/components/Fish.js
+++ app/src/components/Fish.js
@@ -3,21 +3,18 @@ import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
     render() {
-        const image = this.props.details.image;
-        const name = this.props.details.name;
-        const price = this.props.details.price;
-        const desc = this.props.details.desc;
-        const status = this.props.details.status;
+        const { image, name, price, desc, status } = this.props.details;
         return (
             <li className="menu-fish">
-               <img src={image} alt={name}/>
-               <h3 className="fish-name">{name} 
-                   <span className="price">{formatPrice(price)}</span>
-               </h3>
-               <p>{desc}</p>
-               <button>Add to Cart</button>
+                <img src={image} alt={name} />
+                <h3 className="fish-name">
+                    {name}
+                    <span className="price">{formatPrice(price)}</span>
+                </h3>
+                <p>{desc}</p>
+                <button>Add to Cart</button>
             </li>
-        )
+        );
     }
 }
 
