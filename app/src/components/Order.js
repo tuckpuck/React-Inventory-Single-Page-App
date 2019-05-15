97170fc914e55e20e85414d3c4aa77cf5fbaf774
tuckpuck <tucker.triggs@yahoo.com>
2019-05-15 07:32:25 -0600

    passing order state to order component

--- app/src/components/Order.js
+++ app/src/components/Order.js
@@ -2,10 +2,25 @@ import React from 'react';
 
 class Order extends React.Component {
     render() {
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status === 'available';
+            if (isAvailable) {
+                return prevTotal + (count + fish.price);
+            }
+            return prevTotal;
+        }, 0);
+
         return (
-            <div className="order">
-            order!!!</div>
-        )
+            <div className="order-wap">
+                <h2>Order</h2>
+                <div className="total">
+                    <strong>{total}</strong>
+                </div>
+            </div>
+        );
     }
 }
 
