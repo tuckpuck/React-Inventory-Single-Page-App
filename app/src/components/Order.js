832182c051810e65059e8aa778846cc6f793d519
tuckpuck <tucker.triggs@yahoo.com>
2019-05-15 07:42:26 -0600

    refactored internal functions to thier own functions

--- app/src/components/Order.js
+++ app/src/components/Order.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
+    renderOrder = (key) => {
+        return <li>{key}</li>;
+    };
+
     render() {
         const orderIds = Object.keys(this.props.order);
         const total = orderIds.reduce((prevTotal, key) => {
@@ -17,6 +21,7 @@ class Order extends React.Component {
         return (
             <div className="order-wap">
                 <h2>Order</h2>
+                <ul>{orderIds.map(this.renderOrder)}</ul>
                 <div className="total">
                     Total:
                     <strong> {formatPrice(total)}</strong>
