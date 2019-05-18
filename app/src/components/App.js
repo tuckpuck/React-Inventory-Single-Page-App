72e049bca678c18ebc707218928cb1baa3c6a90b
tuckpuck <tucker.triggs@yahoo.com>
2019-05-18 09:46:31 -0600

    syncing state to firebase through App

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -4,12 +4,21 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
     state = {
         fishes: {},
         order: {}
     };
+
+    componentDidMount() {
+        const { params } = this.props.match;
+        this.ref = base.syncState(`${params.storeId}/fishes`, {
+            context: this,
+            state: 'fishes'
+        });
+    }
 
     addFish = (fish) => {
         // Make copy of existing state
