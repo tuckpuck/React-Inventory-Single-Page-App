4bfc46cc76ca09d397c1993b12c707591ad942f9
tuckpuck <tucker.triggs@yahoo.com>
2019-05-30 10:59:21 -0600

    added proptypes to login

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Login = () => (
     <nav className="login">
@@ -8,6 +9,10 @@ const Login = () => (
         <button className="twitter" onclick={()=>this.props.authenticate('Twitter')}>Log in with Twitter</button>
         <button className="facebook" onclick={()=>this.props.authenticate('Facebook')}>Log in with Facebook</button>
     </nav>
-)
+);
+
+Login.propTypes = {
+    authenticate: PropTypes.func.isRequired
+};
 
 export default Login;
