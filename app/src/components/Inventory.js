33cff4345f3a3ea8a871a39235c5ff38af7d6076
tuckpuck <tucker.triggs@yahoo.com>
2019-06-05 11:51:51 -0600

    added logout button and logout functionality

--- app/src/components/Inventory.js
+++ app/src/components/Inventory.js
@@ -43,8 +43,16 @@ class Inventory extends React.Component {
       const authProvider = new firebase.auth[`${provider}AuthProvider`]();
       firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
   }
+
+  logout = async () => {
+    console.log('logging out');
+    await firebase.auth().signOut();
+    this.setState({uid:null});
+  }
   
     render() {
+      const logout = <button onClick={this.logout}>Log Out!</button>
+
       // Check if they are logged in
       if (!this.state.uid) {
           return <Login authenticate={this.authenticate} />;
@@ -54,6 +62,7 @@ class Inventory extends React.Component {
       if(this.state.uid !== this.state.owner){
         return <div>
           <p>Sorry, you are not the owner</p>
+          {logout}
         </div>
       }
 
@@ -61,6 +70,7 @@ class Inventory extends React.Component {
         return (
             <div className="Inventory">
                 <h2>Inventory</h2>
+                {logout}
                 {Object.keys(this.props.fishes).map((key) => (
                     <EditFishForm key={key} index={key} fish={this.props.fishes[key]} updateFish={this.props.updateFish} deleteFish={this.props.deleteFish} />
                 ))}
